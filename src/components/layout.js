8dd37e01dfb30428c10792204d204bf31e881040
Christoph1911 <68999214+Christoph1911@users.noreply.github.com>
2020-08-05 11:51:39 +0000

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ const ListLink = props => (
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem`,background-color: lavenderblush}}>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`, backgroundColor: `#FFD35D`}}>
           <h3 style={{ display: `inline` }}>Christoph Paltzer</h3>
