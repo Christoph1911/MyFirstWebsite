a939180293dfe56c634463e844e4089e1e8b7033
Christoph1911 <christophpaltzer@gmail.com>
2020-08-05 18:19:48 +0200

    "Changed About-Page"

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ export default function About() {
     <Layout>
       <h1>About me</h1>
       <p>
-        I’m good enough, I’m smart enough, and gosh darn it, people like me!
+        This is my first Website!
       </p>
     </Layout>
   )
