6380af2cf6592dff6c49830c58bb50338e14789f
Christoph1911 <christophpaltzer@gmail.com>
2020-08-06 13:35:28 +0200

    c

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,15 @@ export default function About() {
       <p>
         Currently I'm in high school and go in the 11th grade.
         I've been on a two week student exchange in Ingatestone (a small town near London), which was a very cool and interesting experience.
-        I've done a one week internship at <a href="https://flowkey.breezy.hr">flowkey</a>, a startup in Berlin.
+        I've done a one week internship at <a href="https://flowkey.breezy.hr">flowkey</a>, a startup in Berlin, where I learned to build this website and other things.
+      </p>
+      <p>
+        I'm interested in programming and have tried a few different programming languages: C#, Java, Javascript, Python. I got into programming in school, where I started eperimenting with the Arduino.
+        Other hobbys are Karate and playing the saxophone. 
+      </p>
+      <p>
+        But my gratest passion is speedcubing (You try to solve the rubik's cube and other twisty puzzles as quickly as possible).
+        Here you can view my <a href="https://www.worldcubeassociation.org/persons/2017PALT01">WCA profile</a>.
       </p>
     </Layout>
   )
